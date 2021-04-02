0ecb567be0a3e7e291bac33b27dec3a53df6ce9e
Chris Martinez <cbmartinez42@gmail.com>
2021-04-01 21:31:28 -0500

    updating with some js added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,29 +10,75 @@ const questions = [
         type: 'input',
         message: 'What is the title of your project?',
         name: 'title'
-    }
+    },
     {
         type: 'input',
         message: 'Please enter a description of your project:',
         name: 'description'
-    }
+    },
     {
         type: 'input',
         message: 'What are the installation instructions?',
         name: 'instructions'
-    }
+    },
     {
         type: 'input',
         message: 'How is this application to be used?',
         name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'What license should be applied to this application?',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0 License', 'GNU GPL v3', 'IBM Public License Version 1.0', 'Mozilla Public License 2.0', 'None'],
+        default: 0,
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who contributed to this project"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+    err ? console.log(err) : console.log('Success!') }
+);
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+
+        .then(responses => {
+            
+            console.log(responses);
+            const readmeString = generateMarkdown(responses);
+            // const generateMarkdown = generateMarkdown(responses);
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // use switch or if
+  let badge;
+  console.log('badge is', badge);
+  console.log(license.toLowerCase() == 'mit')
+//   if(license.toLowerCase() == 'mit')
+//     return 'MIT Badge String'
+// } else if(license.toLowerCase() === ){
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,10 +20,34 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(responses) {
+  return `# ${responses.title.toUpperCase()}
+  ${renderLicenseBadge(responses.license)}
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributors](#contributors)
+    - [Tests](#tests)
+    - [Questions](#questions)
 
-`;
-}
+    ## Description
+    
+    ${responses.description}
+    ## Installation Instructions
+    To install this application, please follow the below instructions:
+    ${responses.instructions}
+    ## Usage
+    ${responses.usage}
+    ## License
+    This application is covered under the ${responses.license}
+    
+    ## Contributors
+    Many thanks to those who contributed to this project:
+    $
+
+`
+};
 
 module.exports = generateMarkdown;
